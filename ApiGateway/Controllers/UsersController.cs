780527e298973d6c154a95ea4863c26e0d4d906a
ensarhamzic <ensarhamzic01@gmail.com>
2024-01-01 19:53:49 +0100

    add and basic setup api gateway

---
+++ ApiGateway/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ApiGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly HttpClient httpClient;
+        private readonly Urls url;
+
+        public UsersController(IHttpClientFactory httpClientFactory, IOptions<Urls> config)
+        {
+            httpClient = httpClientFactory.CreateClient();
+            url = config.Value;
+        }
+
+        [HttpGet("drivers/{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var urlPath = url.UsersManagement + $"/Users/drivers/{id}";
+            var request = new HttpRequestMessage(HttpMethod.Get, urlPath);
+            var response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            return BadRequest(response.Content.ReadAsStringAsync());
+        }
+
+    }
+}
---
+++ ApiGateway/Urls.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiGateway
+{
+    public class Urls
+    {
+        public string UsersManagement { get; set; }
+        public string RoutesManagement { get; set; }
+    }
+}
