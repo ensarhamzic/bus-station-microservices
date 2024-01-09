4f514caf3624a24fb71d22e2ca595fb4c1d7a4c8
ensarhamzic <ensarhamzic01@gmail.com>
2024-01-09 20:34:20 +0100

    add more routes

--- ApiGateway/Routes/TicketRoutes.cs
+++ ApiGateway/Routes/TicketRoutes.cs
@@ -3,6 +3,8 @@
     public static class TicketRoutes
     {
         public const string TicketBaseRoute = "/tickets";
-        public const string BuyTicketRoute = $"{TicketBaseRoute}";
+        public const string BuyTicketRoute = $"{TicketBaseRoute}/buy";
+        public const string BookTicketRoute = $"{TicketBaseRoute}/book";
+        public const string ConfirmTicketRoute = $"{TicketBaseRoute}/confirm";
     }
 }
